1abea0afdeed4672c34b5a06a4fa8755fe5b7089
bpereira615 <bpereira615@gmail.com>
2016-03-23 15:26:13 -0400

    Ready to submit

--- melody_creation_driver.m
+++ melody_creation_driver.m
@@ -15,7 +15,6 @@ generate_melody_volume('melody3.mat', 1);
 
 
 
-
 %generation of melodies and differnet speeds, harmonic enrichment
 generate_melody_harmonics('melody1.mat', 1);
 generate_melody_harmonics('melody2.mat', 1);
